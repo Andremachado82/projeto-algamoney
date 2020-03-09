c2105d02dc46df65838cbb8caec447804a5df52f
Andremachado82 <andrels.machado82@gmail.com>
2020-03-09 17:12:27 -0300

    Inserindo Repository, Resource e model de pessoa

---
+++ algamoney-api/src/main/java/com/algamoney/api/resource/PessoaResource.java
@@ -0,0 +1,56 @@
+package com.algamoney.api.resource;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.algamoney.api.model.Pessoa;
+import com.algamoney.api.repository.PessoaRepository;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaResource {
+	
+	@Autowired
+	private PessoaRepository pessoaRepository;
+	
+//	cria uma pessoa	
+	@PostMapping
+	public ResponseEntity<Pessoa> criar(
+			 @Valid @RequestBody Pessoa pessoa, HttpServletResponse response) {
+		Pessoa pessoaSalva = pessoaRepository.save(pessoa);
+		
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri()
+				.path("/{codigo}")
+				.buildAndExpand(pessoaSalva.getCodigo()).toUri();
+		response.setHeader("Location", uri.toASCIIString());
+		
+		return ResponseEntity.created(uri).body(pessoaSalva);
+	}	
+	
+//	lista todas as pessoas cadastradas
+	@GetMapping
+	public List<Pessoa> listar() {
+		return pessoaRepository.findAll();
+	}
+	
+	@GetMapping("/{codigo}")
+	public ResponseEntity<?> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<?> pessoa = pessoaRepository.findById(codigo);
+		
+		 return pessoa != null ? ResponseEntity.ok(pessoa) : ResponseEntity.notFound().build();
+	}
+}
